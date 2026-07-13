84a9af9a8c62f1226491f2b1441fcf27ba1e2df5
PARK SINYOUNG <park_new0@naver.com>
2026-07-13 16:17:24 +0900

    Initial Commit

---
+++ GuitarSync/GuitarSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarSyncApp.swift
+//  GuitarSync
+//
+//  Created by Park on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct GuitarSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
